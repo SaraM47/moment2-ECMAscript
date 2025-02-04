52004891208fb6356f08a012807675b277d46e67
SaraMsoso <Sara.soder44@gmail.com>
2025-02-04 20:52:51 +0100

    Skriver in asynkron kod, genom att påbörja FetchAPI tillsammans med async/await och try/catch.

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,22 @@ document.addEventListener("DOMContentLoaded",()=> {
 
 let coursesData = [];
 let sortOrder = 1; 
+
+async function fetchAndDisplayCourses() {
+    const url = "https://webbutveckling.miun.se/files/ramschema_ht24.json";
+
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error("Fel vid anslutning till data...");
+        }
+
+        coursesData = await response.json();
+        displayCourses(coursesData);
+
+    } catch (error) {
+        console.error(error);
+        document.querySelector("#error").innerHTML =
+      "<p>Fel vid anslutning - prova igen senare!</p>";
+    }
+}
