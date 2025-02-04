55c7efb7183dafbd0eda97e35828372abf134c86
SaraMsoso <Sara.soder44@gmail.com>
2025-02-04 20:25:07 +0100

    Lagt in händelsehanterare för sortering

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded",()=>
-{
+document.addEventListener("DOMContentLoaded",()=> {
     fetchAndDisplayCourses();
 
     document.getElementById("search").addEventListener("input", filterCourses);
-})
+
+    document.querySelectorAll(".sortable").forEach(header => {
+        header.addEventListener("click", () => sortCourses(header.dataset.column));
+    });
+});
+
